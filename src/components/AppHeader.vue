1a73bcecab875aed44c56d5f23ffd3fafd15a50a
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-02-16 01:08:14 +0100

    feat serie/movie filter

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -18,13 +18,34 @@ export default {
       store.tmdbQuary = text;
       store.fetchFilms();
     },
+
+    handleClickHome() {
+      store.filter.serie = false;
+      store.filter.movie = false;
+      store.tmdbQuary = "";
+    },
+
+    handleClickSerie() {
+      store.filter.serie = true;
+      store.filter.movie = false;
+    },
+
+    handleClickMovie() {
+      store.filter.serie = false;
+      store.filter.movie = true;
+    },
   },
 };
 </script>
 
 <template>
   <header class="container">
-    <app-logo />
+    <div class="menu">
+      <app-logo class="logo" />
+      <div @click="handleClickHome()" class="menu-opt" :class="!store.filter.serie && !store.filter.movie ? 'active' : ''">Home</div>
+      <div @click="handleClickSerie()" class="menu-opt" :class="store.filter.serie ? 'active' : ''">Serie Tv</div>
+      <div @click="handleClickMovie()" class="menu-opt" :class="store.filter.movie ? 'active' : ''">Film</div>
+    </div>
     <search-bar @searching="searchFilms" class="searchbar" />
   </header>
 </template>
@@ -36,5 +57,28 @@ header {
   justify-content: space-between;
   align-items: center;
   font-size: 1.5rem;
+
+  .menu {
+    display: flex;
+    align-items: center;
+    gap: 30px;
+    height: 100%;
+    font-size: 1.2rem;
+    .logo {
+      margin-right: 20px;
+    }
+
+    .menu-opt {
+      color: #575757;
+      cursor: pointer;
+      // height: 100%;
+      line-height: 50px;
+
+      &.active {
+        color: white;
+        box-shadow: 0 5px 0 white;
+      }
+    }
+  }
 }
 </style>
